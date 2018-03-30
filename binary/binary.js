02679ec393d8ba0a8a9b6f7a18e3aabf82278f50
Jardine <jardine.miller@gmail.com>
2018-03-30 14:36:28 +0100

    binary complete

--- binary/binary.js
+++ binary/binary.js
@@ -4,15 +4,17 @@ class Binary {
   }
 
   toDecimal() {
+    if(this.binary.match(/[^01]/)) return 0;
     if(this.binary == 1) return 1;
-    let length = this.binary.length;
-    let thing = this.binary.split("").map((each, index) => {
-      return each * Math.pow(2, length - index);
-    }).reduce((acc, currentVal) => {
-      return acc + currentVal;
-    })
-    // console.log(thing);
-    return thing;
+    let length = this.binary.length - 1;
+    return this.binary
+      .split("")
+      .map((each, index) => {
+        return each * Math.pow(2, length - index);
+      })
+      .reduce((acc, currentVal) => {
+        return acc + currentVal;
+      })
   }
 
 }
