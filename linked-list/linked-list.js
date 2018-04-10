520dc592cb92857b8ca74ad25bd84632da4a67b4
Jardine <jardine.miller@gmail.com>
2018-04-10 13:02:24 +0100

    linked list completed

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -12,6 +12,10 @@ class LinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  count() {
+    return this.length;
+  }
   
   push(number) {
     let newNode = new Node(number);
@@ -67,6 +71,35 @@ class LinkedList {
       this.length--;
     }
     return removed.value;
+  }
+
+  delete(number) {
+    if(this.length == 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.previous;
+      this.tail.next = null;
+    }
+    this.length--;
+  }
+
+  delete(number) {
+    let node = this.head;
+    if(this.length == 1) {
+      this.head = null;
+      this.tail = null;
+      this.length--;
+      return;
+    }
+    for(let i = 0; i <= this.length; i++) {
+      if(node.value == number) {
+        node.previous.next = node.next;
+        node.next.previous = node.previous;
+        this.length--;
+      }
+      node = node.next;
+    }
   }
 
 }
